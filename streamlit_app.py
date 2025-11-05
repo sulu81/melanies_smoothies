c0ac5420a4f60f1fe1bca5a5bfe9232f9c9eef02
sulu81 <sulakshana06@yahoo.com>
2025-11-05 22:24:44 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -40,6 +40,9 @@ if ingredients_list:
 
    for each_fruit in ingredients_list:
        ingredients_string += each_fruit + ' '
+       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
+       st.write('The search value for ', each_fruit,' is ', search_on, '.')
+     
        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+each_fruit)
        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
